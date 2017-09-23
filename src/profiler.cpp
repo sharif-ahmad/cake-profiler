a035328bfef0357059bd5fd24b6bafb5cfe85127
Sharif Ahmad <sharifrah@gmail.com>
2017-09-24 00:57:55 +0600

    #2 add suuport for function call frequency tracker.

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -7,11 +7,13 @@ namespace poc
 Profiler::Profiler(const std::string& name, ProfilerManager& manager)
     : start(clock::now()), name(name), manager(manager)
 {
+    manager.addCallTime(name, start);
 }
 
 Profiler::Profiler(std::string&& name, ProfilerManager& manager)
     : start(clock::now()), name(std::move(name)), manager(manager)
 {
+    manager.addCallTime(this->name, start);
 }
 
 Profiler::~Profiler()
